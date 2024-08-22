15d004fb48b6d4c3b167c8a7812775548279afc4
sanjayrushee <rashmikafan05.com>
2024-08-22 23:30:36 +0530

    80 percentage completed

--- app.js
+++ app.js
@@ -28,20 +28,55 @@ const initializeDbAndServer = async () => {
 initializeDbAndServer();
 
 
-app.post("/register/",async(request,response) =>{
-  const {name,student_id,phonenumber,area_of_interest,eamil,availability,mentor_name,slot_one} = request.body
+app.post("/saveuser/",async(request,response) =>{
+  const {name,student_id,phonenumber,area_of_interest,eamil,availability,mentor_name,mentor_availability} = request.body
 
-  const getUser = `SELECT * FROM student WHERE name = ? AND slot_booked = 'yes';`;
-  const checktheUser = await database.get(getUser, [name]);
+  const getUser = `SELECT * FROM student WHERE name = ? AND slot_booked = 'yes' AND student_id = ? ;`;
+  const checktheUser = await database.get(getUser, [name,student_id]);
 
   if (checktheUser) {
       console.log('User has already booked a slot');
       return response.status(400).json({ message: "User has already booked a slot" });
   } else {
       const setStudent = `INSERT INTO student VALUES('${student_id}','${name}','${availability}','${area_of_interest}','${phonenumber}','${eamil}','yes');`
-      const setMentor = 'INSERT INTO mentor VALUES'
+      const setMentor = 'UPDATE mentor SET mentor_availability = ? WHERE mentor_name = ?;'
+
       await database.run(setStudent)
+      await database.run(setMentor,[mentor_availability,mentor_name])
       console.log("user added")
+      return response.status(201).json({ message: "user added" });
+
   }
 
+});
+
+
+app.post("/booking/",async(request,response) =>{
+  const {booking_id,student_id,mentor_availability,mentor_id} = request.body
+    try{
+      const setMentor = 'UPDATE mentor SET mentor_availability = ? WHERE mentor_id = ?;'
+      await database.run(setMentor,[mentor_availability,mentor_id])
+      const classbooking = `INSERT INTO booking VALUES(?,?,?);`
+      await database.run(classbooking,[booking_id,student_id,mentor_id])
+      return response.status(201).json({ message: "booked" });
+    }
+    catch (error){
+      console.error(error)
+      return response.status(500).json({message:error})
+    }
+
+});
+
+app.get("/mentor/", async(request,response) =>{
+  const {area_of_expertise} = request.query
+try{
+  const getmentors = `SELECT * FROM mentor WHERE area_of_expertise = ? ORDER BY feedback ASC;`
+  const mentorDetails = await database.all(getmentors,[area_of_expertise])
+  console.log(mentorDetails)
+  return response.status(200).send(mentorDetails)
+}
+catch(error){
+  console.error(error)
+  return response.status(400).json({message: "failed"})
+}
 });
--- checking.js
+++ checking.js
@@ -19,7 +19,54 @@ const register = (userData) => {
 
 // Example usage
 const newUser = {
-    name :'hello',student_id :123123,phonenumber: 3423434343,area_of_interest: 'bnoting',eamil: "b;oo@ga",availability: "78pm to 34pm"
+    name :'dsfa',student_id :33434,phonenumber: 3423434343,area_of_interest: 'bnoting',eamil: "b;oo@ga",availability: "78pm to 34pm",
+    mentor_availability:'noting',mentor_name:'Vikram Mehta'
 };
 
-register(newUser);
+//register(newUser);
+
+// Function definition
+const booking = (userData) => {
+    const url = 'http://localhost:3004/booking/';
+    
+    axios.post(url, userData, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        console.log('Response:', response.data);
+    })
+    .catch(error => {
+        console.error('There was an error!', error);
+    });
+}
+
+const newbooking = {
+    booking_id: 2323,student_id:33434,mentor_availability:'bot',mentor_id:9
+};
+
+//booking(newbooking)
+
+const mentor = (userData) => {
+    const url = 'http://localhost:3004/mentor/';
+    
+    axios.get(url, {
+        params: userData, // Pass the data as query parameters
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        console.log('Response:', response.data);
+    })
+    .catch(error => {
+        console.error('There was an error!', error);
+    });
+}
+
+const getmentor = {
+    area_of_expertise: 'Consulting'
+};
+
+mentor(getmentor);
